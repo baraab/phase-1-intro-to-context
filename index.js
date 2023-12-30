bd74b57635eee683d9abc1a492d365fde4e4992a
baraa janobi hamad sidair <baraasedair94@gmail.com>
2023-12-30 13:56:32 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,66 @@
 // Your code here
+
+function createEmployeeRecord(data) {
+    return {
+      firstName: data[0],
+      familyName: data[1],
+      title: data[2],
+      payPerHour: data[3],
+      timeInEvents: [],
+      timeOutEvents: [],
+    };
+  }
+  
+
+  function createEmployeeRecords(arrayOfArrays) {
+    return arrayOfArrays.map(createEmployeeRecord);
+  }
+  
+ 
+  function createTimeInEvent(employee, dateTime) {
+    const [date, hour] = dateTime.split(' ');
+    employee.timeInEvents.push({ type: 'TimeIn', date, hour: parseInt(hour, 10) });
+    return employee;
+  }
+  
+
+  function createTimeOutEvent(employee, dateTime) {
+    const [date, hour] = dateTime.split(' ');
+    employee.timeOutEvents.push({ type: 'TimeOut', date, hour: parseInt(hour, 10) });
+    return employee;
+  }
+  
+  
+  function hoursWorkedOnDate(employee, date) {
+    const timeIn = employee.timeInEvents.find(event => event.date === date);
+    const timeOut = employee.timeOutEvents.find(event => event.date === date);
+    return (timeOut.hour - timeIn.hour) / 100;
+  }
+  
+ 
+  function wagesEarnedOnDate(employee, date) {
+    const hoursWorked = hoursWorkedOnDate(employee, date);
+    return hoursWorked * employee.payPerHour;
+  }
+  
+ 
+  function allWagesFor(employee) {
+    const allDates = employee.timeInEvents.map(event => event.date);
+    return allDates.reduce((totalWages, date) => totalWages + wagesEarnedOnDate(employee, date), 0);
+  }
+  
+  
+  function calculatePayroll(employees) {
+    return employees.reduce((totalPayroll, employee) => totalPayroll + allWagesFor(employee), 0);
+  }
+  
+ 
+  const employeeRecords = createEmployeeRecords([
+    ["Thor", "Odinsson", "Electrical Engineer", 45],
+    ["Loki", "Laufeysson-Odinsson", "HR Representative", 35],
+   
+  ]);
+  
+  const totalPayroll = calculatePayroll(employeeRecords);
+  console.log(totalPayroll); 
+  
